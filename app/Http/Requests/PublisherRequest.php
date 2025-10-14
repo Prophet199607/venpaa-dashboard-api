6b17993d471645a124f8331dc6c6591a1512bf4c
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-10-14 09:18:36 +0530

    Publisher code rules modified

--- app/Http/Requests/PublisherRequest.php
+++ app/Http/Requests/PublisherRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class PublisherRequest extends FormRequest
@@ -23,13 +24,19 @@ class PublisherRequest extends FormRequest
      */
     public function rules()
     {
+        $publisherCode = $this->route('pub_code');
+
         return [
-            'pub_code' => 'required|string',
+            'pub_code' => [
+                'required',
+                'string',
+                Rule::unique('publishers', 'pub_code')->ignore($publisherCode, 'pub_code'),
+            ],
             'pub_name' => 'required|string',
             'pub_image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'website' => 'nullable|string',
             'contact' => 'nullable|string',
-            'email' => 'nullable|string',
+            'email' => 'nullable|string|email',
             'description' => 'nullable|string',
         ];
     }
