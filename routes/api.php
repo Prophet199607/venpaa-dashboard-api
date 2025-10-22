bf71aee33010f77026e5d8b92f172a9565bb945a
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-10-22 14:29:17 +0530

    books route change to products route

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\LocationController;
 use App\Http\Controllers\BookTypeController;
 use App\Http\Controllers\CategoryController;
@@ -125,14 +125,14 @@ Route::group(['prefix' => 'v1', 'middleware' => ['auth:sanctum']], function () {
         Route::put('/{auth_code}', [AuthorController::class, 'update']);
     });
 
-    // book routes
-    Route::group(['prefix' => 'books'], function () {
-        Route::get('/generate-code', [BookController::class, 'generateBookCode']);
-        Route::get('/', [BookController::class, 'index']);
-        Route::get('/search', [BookController::class, 'search']);
-        Route::get('/{book_code}', [BookController::class, 'show']);
-        Route::post('/', [BookController::class, 'store']);
-        Route::put('/{book_code}', [BookController::class, 'update']);
+    // product routes
+    Route::group(['prefix' => 'products'], function () {
+        Route::get('/generate-code', [ProductController::class, 'generateProductCode']);
+        Route::get('/', [ProductController::class, 'index']);
+        Route::get('/search', [ProductController::class, 'search']);
+        Route::get('/{prod_code}', [ProductController::class, 'show']);
+        Route::post('/', [ProductController::class, 'store']);
+        Route::put('/{prod_code}', [ProductController::class, 'update']);
     });
 
     // Role and Permission routes
