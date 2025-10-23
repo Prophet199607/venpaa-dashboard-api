c1f173dd3656921a0bc58ccef1c34a0417241098
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-10-23 14:07:57 +0530

    Move resource files into the Master folder

---
+++ app/Http/Resources/Master/BookTypeResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Master;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookTypeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'bkt_code'     => $this->bkt_code,
+            'bkt_name'     => $this->bkt_name,
+            'created_by'    => $this->created_by,
+            'updated_by'    => $this->updated_by,
+        ];
+    }
+}
---
+++ app/Http/Resources/Master/SubCategoryResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources\Master;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SubCategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'scat_code'     => $this->scat_code,
+            'scat_name'     => $this->scat_name,
+            'department'    => $this->department,
+            'cat_code'      => $this->cat_code,
+            'category' => $this->whenLoaded('category', function () {
+                return [
+                    'id' => $this->category->id,
+                    'cat_code' => $this->category->cat_code,
+                    'cat_name' => $this->category->cat_name,
+                    'department' => $this->category->department,
+                ];
+            }),
+            'created_by'    => $this->created_by,
+            'updated_by'    => $this->updated_by,
+        ];
+    }
+}
