8457cdc0d38d7b3ec3263386fa8330382c609fb9
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-11-18 16:38:35 +0530

    Add item transaction header & detail models with migrations

---
+++ database/migrations/2025_11_18_105225_create_item_transaction_details.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemTransactionDetails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_transaction_details', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('item_transaction_header_id');
+            $table->string('doc_no');
+            $table->integer('line_no');
+            $table->string('iid');
+            $table->string('prod_code');
+            $table->string('prod_name');
+            $table->decimal('purchase_price')->default(0);
+            $table->decimal('marked_price')->default(0);
+            $table->decimal('selling_price')->default(0);
+            $table->decimal('whole_sale')->default(0);
+            $table->decimal('pack_qty',8,3)->default(0)->nullable();
+            $table->decimal('unit_qty',8,3)->default(0)->nullable();
+            $table->decimal('free_qty',8,3)->default(0)->nullable();
+            $table->decimal('physical_pack_qty',8,3)->default(0)->nullable();
+            $table->decimal('physical_unit_qty',8,3)->default(0)->nullable();
+            $table->decimal('total_qty',8,3)->default(0);
+            $table->decimal('physical_total_qty',8,3)->default(0);
+            $table->decimal('pack_size')->default(0);
+            $table->decimal('discount')->default(0);
+            $table->decimal('line_wise_discount_value')->default(0);
+            $table->decimal('dis_per')->default(0);
+            $table->decimal('amount')->default(0);
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_transaction_details');
+    }
+}
---
+++ database/migrations/2025_11_18_105246_create_item_transaction_headers.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemTransactionHeaders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_transaction_headers', function (Blueprint $table) {
+            $table->id();
+            $table->string('location');
+            $table->string('doc_no');
+            $table->string('temp_doc_no')->nullable();
+            $table->date('document_date')->nullable();
+            $table->date('expected_date')->nullable();
+            $table->string('transaction_date')->nullable();
+            $table->string('grn_date')->nullable();
+            $table->string('iid');
+            $table->string('supplier_code')->default(0);
+            $table->text('delivery_address')->nullable();
+            $table->string('delivery_location')->nullable();
+            $table->string('ref_number')->nullable()->default(0);
+            $table->string('remarks_ref')->nullable();
+            $table->string('grn_remarks')->nullable();
+            $table->string('srn_remarks')->nullable();
+            $table->decimal('subtotal')->default(0);
+            $table->decimal('net_total')->default(0);
+            $table->decimal('discount')->default(0);
+            $table->decimal('dis_per')->default(0);
+            $table->decimal('tax_per')->default(0);
+            $table->decimal('tax')->default(0);
+            $table->string('grn_no')->nullable();
+            $table->string('payment_mode')->nullable();
+            $table->string('invoice_no')->nullable();
+            $table->date('invoice_date')->nullable();
+            $table->decimal('invoice_amount')->default(0);
+            $table->string('approval_status')->default('pending');
+            $table->boolean('is_approved')->default(0);
+            $table->string('approved_by')->nullable();
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_transaction_headers');
+    }
+}
