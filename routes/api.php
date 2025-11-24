826c4f79e879e7dfbfb89a93f666e1827314f450
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-11-24 12:07:32 +0530

    Add endpoint to retrieve locations in API routes

--- routes/api.php
+++ routes/api.php
@@ -41,6 +41,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
+Route::get('/locations', [LocationController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
