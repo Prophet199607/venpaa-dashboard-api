272de8396d352e8616766011436d720e522c3c33
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-11-26 16:16:02 +0530

    Remove 'industry_code' column and comments from stock_masters migration for cleaner schema definition.

--- database/migrations/2025_11_25_173121_create_stock_masters_table.php
+++ database/migrations/2025_11_25_173121_create_stock_masters_table.php
@@ -15,21 +15,15 @@ class CreateStockMastersTable extends Migration
     {
         Schema::create('stock_masters', function (Blueprint $table) {
         $table->id();
-        $table->string('industry_code');
         $table->string('location');
         $table->string('transaction_date')->nullable();
         $table->string('doc_no');
         $table->string('prod_code');
         $table->string('iid');
-
-        // Quantity with up to 8 digits total and 3 decimal places
         $table->decimal('qty', 8, 3)->default(0.000);
-
-        // Prices and amounts with large decimal capacity
         $table->decimal('purchase_price', 20, 2)->default(0.00);
         $table->decimal('selling_price', 20, 2)->default(0.00);
         $table->decimal('amount', 20, 2)->default(0.00);
-
         $table->timestamps();
         });
     }
