88486699107d278e8ff4a3930403c9a7a5709c69
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-11-29 12:35:18 +0530

    Add SupplierReturnNoteController and SupplierReturnNote model for handling supplier return notes

---
+++ app/Http/Controllers/Transaction/SupplierReturnNoteController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Transaction;
+
+use App\Models\DocNumber;
+use App\Http\Controllers\Controller;
+
+class SupplierReturnNoteController extends Controller
+{
+
+}
