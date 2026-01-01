4f25854bf07609bf334ea2415d813ae6e4f75e4d
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-01-01 12:26:30 +0530

    Add AuthorImport class for Excel-based author data import

---
+++ app/Imports/AuthorImport.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Author;
+use App\Models\DocNumber;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class AuthorImport implements ToModel, WithHeadingRow
+{
+    public function model(array $row)
+    {
+        if (!isset($row['authors_name'])) {
+            return null;
+        }
+
+        $docNumber = DocNumber::where('type', 'Author')->first();
+        
+        $auth_code = 'AUTH-' . time(); // Fallback
+        if ($docNumber) {
+            $codeData = $docNumber->getDocCode();
+            $auth_code = $codeData['code'];
+            // Do NOT increment here as Author::created event handles it
+        }
+
+        return new Author([
+            'auth_code'   => $auth_code,
+            'auth_name'   => $row['authors_name'],
+            'status'     => 1,
+            'created_by' => auth()->id(),
+        ]);
+    }
+}
