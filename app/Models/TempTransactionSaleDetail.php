9a67c9801f16ba1d8d54b4fb53ecb7740b4eba7e
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-01-12 13:30:17 +0530

    Add TempTransactionSaleDetail and TempTransactionSaleHeader models, along with TransactionSaleDetail and TransactionSaleHeader models for transaction management. All models utilize Eloquent's HasFactory and have guarded properties set for mass assignment protection.

---
+++ app/Models/TempTransactionSaleDetail.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TempTransactionSaleDetail extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
