978d76f879a1ee9b887fed6a19e8ef0396770fae
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-01-28 11:51:30 +0530

    Update field name casing in POS sales API validation and mapping

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -16,8 +16,8 @@ class SalesController extends Controller
     public function InsertPosSales(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'details' => 'required|array|min:1',
-            'details.*.r_No' => 'required|string|max:1000',
+            'Details' => 'required|array|min:1',
+            'Details.*.R_No' => 'required|string|max:1000',
         ]);
 
         if ($validator->fails()) {
@@ -28,10 +28,10 @@ class SalesController extends Controller
             ], 422);
         }
 
-        $details = $request->input('details');
+        $details = $request->input('Details');
 
         try {
-            $rNos = collect($details)->pluck('r_No')->unique()->toArray();
+            $rNos = collect($details)->pluck('R_No')->unique()->toArray();
             $existingRNos = PosTransactionApi::whereIn('R_No', $rNos)
                 ->pluck('R_No')
                 ->toArray();
@@ -40,10 +40,10 @@ class SalesController extends Controller
 
             $records = [];
             foreach ($details as $item) {
-                if (in_array($item['r_No'], $existingRNos)) {
+                if (in_array($item['R_No'], $existingRNos)) {
                     continue;
                 }
-                if (collect($records)->pluck('R_No')->contains($item['r_No'])) {
+                if (collect($records)->pluck('R_No')->contains($item['R_No'])) {
                     continue;
                 }
 
@@ -76,31 +76,31 @@ class SalesController extends Controller
     private function storeRecord(array $data)
     {
         $mapped = [
-            'Loca'            => $data['loca'] ?? null,
-            'Iid'             => $data['iid'] ?? null,
-            'Item_Code'       => $data['item_Code'] ?? null,
-            'Item_Descrip'    => $data['item_Descrip'] ?? null,
-            'Unit_Price'      => $data['unit_Price'] ?? null,
-            'Cost_Price'      => $data['cost_Price'] ?? null,
-            'Marked_Price'    => $data['marked_Price'] ?? null,
-            'Qty'             => $data['qty'] ?? null,
-            'Amount'          => $data['amount'] ?? null,
-            'Tr_Type'         => $data['tr_Type'] ?? null,
-            'Receipt_No'      => $data['receipt_No'] ?? null,
-            'SalesMan'        => $data['salesMan'] ?? null,
-            'Discount'        => $data['discount'] ?? null,
-            'Dis'             => $data['dis'] ?? null,
-            'Unit'            => $data['unit'] ?? null,
-            'Customer'        => $data['customer'] ?? null,
-            'BillDate'        => $data['billDate'] ?? null,
-            'BillTime'        => $data['billTime'] ?? null,
-            'ExchangeReceipt' => $data['exchangeReceipt'] ?? null,
-            'UserName'        => $data['userName'] ?? null,
-            'TransactionDate' => $data['transactionDate'] ?? null,
-            'InsertDate'      => $data['insertDate'] ?? null,
-            'R_No'            => $data['r_No'],
-            'PC_ID'           => $data['pC_ID'] ?? null,
-            'BatchCode'       => $data['batch_Code'] ?? null,
+            'BatchCode'       => $data['Batch_Code'] ?? null,
+            'Loca'            => $data['Loca'] ?? null,
+            'Iid'             => $data['Iid'] ?? null,
+            'Item_Code'       => $data['Item_Code'] ?? null,
+            'Item_Descrip'    => $data['Item_Descrip'] ?? null,
+            'Unit_Price'      => $data['Unit_Price'] ?? null,
+            'Cost_Price'      => $data['Cost_Price'] ?? null,
+            'Marked_Price'    => $data['Marked_Price'] ?? null,
+            'Qty'             => $data['Qty'] ?? null,
+            'Amount'          => $data['Amount'] ?? null,
+            'Tr_Type'         => $data['Tr_Type'] ?? null,
+            'Receipt_No'      => $data['Receipt_No'] ?? null,
+            'SalesMan'        => $data['SalesMan'] ?? null,
+            'Discount'        => $data['Discount'] ?? null,
+            'Dis'             => $data['Dis'] ?? null,
+            'Unit'            => $data['Unit'] ?? null,
+            'Customer'        => $data['Customer'] ?? null,
+            'BillDate'        => $data['BillDate'] ?? null,
+            'BillTime'        => $data['BillTime'] ?? null,
+            'ExchangeReceipt' => $data['ExchangeReceipt'] ?? null,
+            'UserName'        => $data['UserName'] ?? null,
+            'TransactionDate' => $data['TransactionDate'] ?? null,
+            'InsertDate'      => $data['InsertDate'] ?? null,
+            'R_No'            => $data['R_No'],
+            'PC_ID'           => $data['PC_ID'] ?? null,
             'Merchant_ID'     => $data['merchant_ID'] ?? null,
             'Merchant_Name'   => $data['merchant_Name'] ?? null,
         ];
