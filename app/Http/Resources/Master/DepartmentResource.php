7aece3d22ebc3a43f9bd46b5240da580e1ff89e1
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-02-25 13:07:32 +0530

    Refactor DepartmentController and DepartmentResource to use S3 for image storage and retrieval. Update image URL generation to provide temporary S3 URLs, enhancing image management capabilities.

--- app/Http/Resources/Master/DepartmentResource.php
+++ app/Http/Resources/Master/DepartmentResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Master;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class DepartmentResource extends JsonResource
 {
@@ -19,9 +20,27 @@ class DepartmentResource extends JsonResource
             'dep_code'     => $this->dep_code,
             'dep_name'     => $this->dep_name,
             'dep_image'    => $this->dep_image,
-            'dep_image_url' => $this->dep_image ? asset('storage/' . $this->dep_image) : null,
+            // 'dep_image_url' => $this->dep_image ? asset('storage/' . $this->dep_image) : null,
+            'dep_image_url' => $this->getS3Url(),
             'created_by'    => $this->created_by,
             'updated_by'    => $this->updated_by,
         ];
+    }
+
+    /**
+     * Get S3 URL with proper type hinting for Intelephense
+     * 
+     * @return string|null
+     */
+    private function getS3Url()
+    {
+        if (!$this->dep_image) {
+            return null;
+        }
+
+        /** @var \Illuminate\Filesystem\FilesystemAdapter $disk */
+        $disk = Storage::disk('s3');
+
+        return $disk->temporaryUrl($this->dep_image, now()->addMinutes(60));
     }
 }
