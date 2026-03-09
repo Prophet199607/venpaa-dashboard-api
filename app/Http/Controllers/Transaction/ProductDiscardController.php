0f0fef9f7a236b4305fb3aa0282b97b81b029681
onimtacc11 <cc11@onimtait.com>
2026-03-09 13:15:39 +0530

    feat: introduce new controllers for various transaction types including supplier returns, good notes, item requests, invoices, and stock adjustments.

--- app/Http/Controllers/Transaction/ProductDiscardController.php
+++ app/Http/Controllers/Transaction/ProductDiscardController.php
@@ -321,12 +321,11 @@ class ProductDiscardController extends Controller
 
                 $headerData = $data;
                 unset($headerData['id']);
-                $transactionHeader = TransactionHeader::create([
-                    ...$headerData,
+                $transactionHeader = TransactionHeader::create(array_merge($headerData, [
                     'doc_no'      => $pdNumber,
                     'temp_doc_no' => $data['doc_no'],
                     'created_by'  => auth()->id(),
-                ]);
+                ]));
 
                 // Load temp products for this temp doc
                 $tempProducts = TempTransactionDetail::where('doc_no', $data['doc_no'])
@@ -337,11 +336,10 @@ class ProductDiscardController extends Controller
                 foreach ($tempProducts as $temp) {
                     $tempData = $temp->toArray();
                     unset($tempData['temp_transaction_header_id'], $tempData['id']);
-                    $transactionDetail = TransactionDetail::create([
-                        ...$tempData,
+                    $transactionDetail = TransactionDetail::create(array_merge($tempData, [
                         'transaction_header_id' => $transactionHeader->id,
                         'doc_no'                => $pdNumber,
-                    ]);
+                    ]));
                     $transactionDetails[] = $transactionDetail;
                 }
 
