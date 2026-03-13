1e1ef33499ad3ceba30868b4d921cf3f52508591
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-03-13 16:15:47 +0530

    feat: Create product_sub_category_l2s table migration.

--- database/migrations/2026_03_13_160000_create_product_sub_category_l2s_table.php
+++ database/migrations/2026_03_13_160000_create_product_sub_category_l2s_table.php
@@ -13,17 +13,15 @@ class CreateProductSubCategoryL2sTable extends Migration
      */
     public function up()
     {
-        if (!Schema::hasTable('product_sub_category_l2s')) {
-            Schema::create('product_sub_category_l2s', function (Blueprint $table) {
-                $table->id();
-                $table->string('prod_code');
-                $table->unsignedBigInteger('sub_category_l2_id');
-                $table->unsignedBigInteger('created_by')->nullable();
-                $table->timestamps();
-                $table->index('prod_code');
-                $table->foreign('sub_category_l2_id')->references('id')->on('sub_category_l2s')->onDelete('cascade');
-            });
-        }
+        Schema::create('product_sub_category_l2s', function (Blueprint $table) {
+            $table->id();
+            $table->string('prod_code');
+            $table->unsignedBigInteger('sub_category_l2_id');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->timestamps();
+            $table->index('prod_code');
+            $table->foreign('sub_category_l2_id')->references('id')->on('sub_category_l2s')->onDelete('cascade');
+        });
     }
 
     /**
