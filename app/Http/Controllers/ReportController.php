9dc71a9f676dea0e3242739ed2253405490f8d91
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-05-27 10:10:45 +0530

    Sales report get by sp_SalesReport_v3 SP

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -230,7 +230,7 @@ class ReportController extends Controller
             }
 
             $pdo = DB::getPdo();
-            $stmt = $pdo->prepare("CALL sp_SalesReport_v4(@pErrorCode, ?, ?, ?, ?, ?, ?)");
+            $stmt = $pdo->prepare("CALL sp_SalesReport_v3(@pErrorCode, ?, ?, ?, ?, ?, ?)");
             $stmt->execute([
                 $location,
                 $dateFrom,
